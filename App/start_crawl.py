5a9d735a27bcfd6cfc4cf947bba96fdd44600a14
Stina Qvarnström <stinaqv@gmail.com>
2015-06-04 15:27:00 +0200

    Starting point of script :watermelon:

---
+++ App/start_crawl.py
@@ -0,0 +1,13 @@
+import requests
+import get_links
+import processLinks
+
+def start (url):
+    domain = 'http://stinaq.se'
+    r = requests.get(url)
+    full_html = r.text
+    all_links = get_links.find_all_links(full_html, domain)
+
+    processLinks.visitLinks(all_links)
+
+start('http://stinaq.se/2015/06/en-sidan-med-massa-trasiga-lankar/')
