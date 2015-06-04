de3f0d463c8651a5f64a58ff54ab5eeff0f89889
Stina Qvarnström <stinaqv@gmail.com>
2015-06-04 15:25:51 +0200

    Saving titles encoded as utf-8 :grapes:

--- App/get_links.py
+++ App/get_links.py
@@ -5,3 +5,19 @@ def make_absolute_of_relative(url, domain):
         return domain + url
     return url
 
+def find_all_links(html, domain):
+    soup = BeautifulSoup(html)
+    a_tags = soup.find_all('a')
+    linkObjects = []
+    for a_tag in a_tags:
+        url = a_tag['href']
+        title = ''.join(a_tag.get_text("|", strip=True)).encode('utf-8')
+
+        absolute_url = make_absolute_of_relative(url, domain)
+
+        temp = {}
+        temp['url'] = absolute_url
+        temp['title'] = title
+
+        linkObjects.append(temp)
+    return linkObjects
