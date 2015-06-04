0dd2a29655d5bcac983fd71c8f4819adf8458e82
Malinils <malin_nilssson@hotmail.com>
2015-06-04 17:26:19 +0200

    Small updates :dancers:
    Final stuff for dev day

--- App/start_crawl.py
+++ App/start_crawl.py
@@ -9,6 +9,6 @@ def start (url):
     full_html = r.text
     all_links = get_links.find_all_links(full_html, domain)
 
-    processLinks.visitLinks(all_links)
+    processLinks.visit_links(all_links)
 
 start('http://stinaq.se/2015/06/en-sidan-med-massa-trasiga-lankar/')
