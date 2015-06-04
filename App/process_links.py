365e993bf7dd4c4dfcf96018304a3ca07a48f17f
Stina Qvarnström <stinaqv@gmail.com>
2015-06-04 22:35:11 +0200

    Outputting file inside project :pear:
    This is to make it work across platforms

--- App/process_links.py
+++ App/process_links.py
@@ -22,3 +22,26 @@ def visit_links(list_of_links):
             print e
     write_to_file(bad_links)
 
+def write_to_file(link_objects):
+    # Getting the current directory
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    dest_dir = os.path.join(script_dir, '..', 'output')
+
+    # Try to open the destination directory, otherwise create it
+    try:
+        os.stat(dest_dir)
+    except:
+        os.mkdir(dest_dir)
+
+    # Create file name with date and time of now
+    file_name = strftime("%Y%m%d_%H-%M-%S", gmtime()) + '.txt'
+
+    file_object = open(os.path.join(dest_dir, file_name), 'a')
+
+    for link in link_objects:
+        #substring title 30 characters
+        title = link['title'][:29]
+
+        #format output in columns
+        file_object.write('{0:30}   {1:30}'.format(title, link['url']) + ' \n')
+    file_object.close()
