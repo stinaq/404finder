6e0f78dbd9f56c4fae77c836f4b0b021f02e16f4
Stina Qvarnström <stinaqv@gmail.com>
2015-06-06 23:41:07 +0200

    A class to hold links :melon:

---
+++ App/link.py
@@ -0,0 +1,14 @@
+class Link(object):
+    """A link object holds the information about links
+
+    Attributes:
+        url: Self explanatory
+        title: The text inside the anchor tag
+        origin: The url to the page where the link was found
+        content_type: The type of the link
+    """
+def __init__(self, url, title, origin):
+    self.url = url
+    self.title = title
+    self.origin = origin
+    
